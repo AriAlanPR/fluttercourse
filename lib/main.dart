e90d077571567484242a98aab5282cb469c3000d
AriAlanPR <shaymin2003@gmail.com>
2022-05-09 16:42:06 -0500

    my first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer X');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color",
+      "What's your favorite animal?",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Answer 2 chosen'),
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: () => print('Answer 3 chosen'),
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
